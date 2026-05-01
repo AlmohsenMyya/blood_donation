921c5435a69720d792a943ee529c6850235082d5
Almohsen Myya <almohsenmyya2233@gmail.com>
2026-05-02 02:12:13 +0300

    NotificationSecrets

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -10,6 +10,8 @@ import 'package:sheryan/l10n/app_localizations.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:googleapis_auth/auth_io.dart' as auth;
 
+import 'notification_secrets.dart';
+
 class NotificationService {
   static final NotificationService _instance = NotificationService._internal();
   factory NotificationService() => _instance;
@@ -32,20 +34,7 @@ class NotificationService {
     importance: Importance.max,
     playSound: true,
   );
-
-  final Map<String, dynamic> _serviceAccount = {
-    "type": "service_account",
-    "project_id": "blood-f5990",
-    "private_key_id": "79d0154416c328a467722005838eba0b53141c9c",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDAgd6TWHtUN3hO\n5msl08VnTs1ce4lJCi/xbE1BkYBrmHeFAbBj5cAo/TwFK3W33ceQs9kHNCRp9/+R\niTGZlmJ4M0Z73vql9vj2cUMNTqw7aVapbsDIoP1F2lljqUp3CRlNUPIgxHieFxEq\nqL/tF7hmuAdkdTylRreF5BS1OyKmlXOIQkjf6UU6EuZKGk1+oACOy4k2JFjuxIPn\nMTwUiIs11igURKzDt35oq5JIx5x5UdQCsbj5KmzVjLwD4Sy+8QSPimzIo+4t9bxX\nKAUgWESAG3Rb7RdZHJY1jPKFlubUCu9khNdp/nKqBqKWHZLQBfQE9AAyuQFQ+ksA\nr+sdfM0HAgMBAAECggEABHsBUyycB48YSGh/K0I34KT/jUd9dSsSMpbytV6i6TOD\nP0qVcGhzMIEJrz/JCEkr0T0JBWyRQbt5QfSPfaOiZxR1Giz2aHvMZO/96jFf5izC\nzO68Te8b9fmUWwggU4+X/H9bI1LEppP4MlCl0ZQxFohHTm9Bb2yrQ30rfjcCIh5j\nW508TzZLdusH7LvckG40SKDVNYi8Lzmf/V/DBc9l9tWfViutWCooehNl94UbbQ4W\nBt1Sm/xuMMEAdA3+6r/86D0BrA8fseKcGP1q2kBmgRrECM1jpiu5lNFaaSWLgCcM\na289Fbzc7i8XR3G7mUQkQWfocgMdU52D9/nk93TqAQKBgQDj+Z5PQj3c9h/ZyZhZ\nytN0viqrcx5vWqf3zbH/pQsinS0Z6FLxPb9dHCjztL/GFcqWKATdyAR+6Pthv3ei\nHvio8+TubkjPg5cO/CIBCvPJUUQhyxvZXp2KFf7XWNH0yKtem+RrvLBLR7llfW0y\nKJa+rKv/Qk3Fpp4luJU4zjANfwKBgQDYLBRS09V5vX0W1c1e15YMdV0zhLX1q03o\n6TP7DYRiLf0N+4zkDalAz+NImx+Cx/lMPXg+xsq2AUpvy9VZt206AOtwTMu4dEbZ\nDyIDsQrYBXP/ngFHOideYb93ZhA/24FCfiqgjE6Z5MJ5wwlgmSgEFACIOarlgEK0\neMxwvYeUeQKBgHfPFZ29yFk5mB+SzNhTubFex3n3NAV9dUzL80HQ8Pst8yfsarqR\nouJCDFuXoDlv9lnXikcr+QDhXEtQnoS7Fh9knemYV/eGxnp/kzdNFDW22cGQxoAE\nM3MAgD+YVC76zuUXtIHSViwZ85scwahcoGxwvquVot2+5NoaGYITCjntAoGAIxbU\nnbVA+6fkfCZsVa7M7mzGmiw6lQwfc2UXSPMiwAUTBIgGkKYfCSQ1kn2LmeD3+IYp\n1JbUJMME4CzIDu4VTssDbJEqqGBHd8hbDxpX1kTcVWvCbVtlNI7NU4Y/sP3id3af\nWLwtrhFR+A3Ood16f173zyT9No+hREYveUVqkpECgYA8r/Oa1HjUuyVXuccavYG4\n39NGpgXUEkjkxnX2Z/OurcBbbtIwwKD/hOAD+hgDLmKZXCiSFjIEuLAkoU/+Lp9+\nV7B8phfmS/3kNRZpTtKkK3Me+soWGw902oeMPsI69TonWtBANtrM/2WM0pwIP1jq\n5VjMJFQ+4RnXZyXCCwaANA==\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-fbsvc@blood-f5990.iam.gserviceaccount.com",
-    "client_id": "101530494291222997393",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40blood-f5990.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-  };
+  final Map<String, dynamic> _serviceAccount = NotificationSecrets.serviceAccount;
 
   Future<String?> _getAccessToken() async {
     try {
